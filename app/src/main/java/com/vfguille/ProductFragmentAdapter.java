ab468c490a54a68894b7d3b63c4913c5af7b7e59
Guillermo Vera <vfguille@gmail.com>
2020-01-14 12:46:25 +0100

    TabLayout

---
+++ app/src/main/java/com/vfguille/ProductFragmentAdapter.java
@@ -0,0 +1,51 @@
+package com.vfguille;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.vfguille.tablayout.ProductDescriptionFragment;
+import com.vfguille.tablayout.ProductInfoFragment;
+import com.vfguille.tablayout.ProductMapFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ProductFragmentAdapter extends FragmentPagerAdapter {
+    private ArrayList<String> title = new ArrayList<>(Arrays.asList("Info", "Description", "Map"));
+    private static final int NUM_ITEMS = 3;
+
+    public ProductFragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                ProductInfoFragment.newInstance();
+            case 1:
+                ProductDescriptionFragment.newInstance();
+            case 2:
+                ProductMapFragment.newInstance();
+            default:
+                return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+}
--- app/src/main/java/com/vfguille/tablayout/ProductActivity.java
+++ app/src/main/java/com/vfguille/tablayout/ProductActivity.java
@@ -1,14 +1,36 @@
 package com.vfguille.tablayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.vfguille.ProductFragmentAdapter;
+
 public class ProductActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private ProductFragmentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tablayout);
+        adapter = new ProductFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
 }
